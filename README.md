69273e29cbe4445eab071fb715edebb0d4cc5830
dieramirezma <dieramirezma@unal.edu.co>
2024-08-08 20:36:50 -0500

    Create repository

---
+++ README.md
@@ -0,0 +1,39 @@
+# Challenge - Encriptador de texto 
+
+En este repositorio se encuentra la solución al primer reto de ALURA LATAM & ONE Oracle Next Education. El reto se basa en una aplicación que encripta textos.
+
+Las "llaves" de encriptación que utilizaremos son las siguientes:
+```
+La letra "e" es convertida para "enter"
+La letra "i" es convertida para "imes"
+La letra "a" es convertida para "ai"
+La letra "o" es convertida para "ober"
+La letra "u" es convertida para "ufat"
+```
+
+- Los requisitos presentados para el resto son los siguientes: 
+
+- Debe funcionar solo con letras minúsculas
+
+- No deben ser utilizados letras con acentos ni caracteres especiales
+
+- Debe ser posible convertir una palabra para la versión encriptada también devolver una palabra encriptada para su versión original.
+
+  Por ejemplo:
+  ```
+  "gato" => "gaitober"
+  gaitober" => "gato"
+  ```
+- La página debe tener campos para inserción del texto que será encriptado o desencriptado, y el usuario debe poder escoger entre las dos opciones.
+
+- El resultado debe ser mostrado en la pantalla.
+
+- Extras:
+
+  Un botón que copie el texto encriptado/desencriptado para la sección de transferencia, o sea que tenga la misma funcionalidad del ctrl+C o de la opción "copiar" del menú de las aplicaciones.
+
+Todos los requisitos anteriores fueron debidamente completados
+
+[Figma del reto](https://www.figma.com/design/trP3p5nEh7XUyB3n2bomjP/Alura-Challenge---Desaf%C3%ADo-1---L%C3%B3gica?node-id=0-1&t=J3eaw8cZjFaQHPC0-0)
+
+[Página web desplegada]()
---
+++ js/script.js
@@ -0,0 +1,85 @@
+function encryptor(message) {
+  const regex = /[aeiou]/g
+  return message.replace(regex, (match) => {
+    switch (match) {
+      case 'a':
+        return 'ai'
+      case 'e':
+        return 'enter'
+      case 'i':
+        return 'imes'
+      case 'o':
+        return 'ober'
+      case 'u':
+        return 'ufat'
+    }
+  })
+}
+
+function decryptor(message) {
+    const regex = /ai|enter|imes|ober|ufat/g
+  return message.replace(regex, (match) => {
+    switch (match) {
+      case 'ai':
+        return 'a'
+      case 'enter':
+        return 'e'
+      case 'imes':
+        return 'i'
+      case 'ober':
+        return 'o'
+      case 'ufat':
+        return 'u'
+    }
+  })
+}
+
+function encryptText() {
+  const $textToEncrypt = document.querySelector('.text-to-encrypt')
+  const regex = /^[a-zñ0-9.,;:!¡¿?()'"\s-]+$/
+  if ($textToEncrypt.value === '') {
+    alert('Por favor, escribe un texto para encriptar.')
+    return
+  }
+  if (!regex.test($textToEncrypt.value)) {
+    alert('Por favor, escribe un texto válido.')
+    return
+  }
+  const encryptedText = encryptor($textToEncrypt.value)
+  const $encryptedTextElement = document.querySelector('.text-encrypted')
+  const $nonEncrypted = document.querySelector('.non-encrypted')
+  const $copyButton = document.querySelector('.copy-button')
+
+  $encryptedTextElement.style.display = 'block'
+  $encryptedTextElement.textContent = encryptedText
+
+  $nonEncrypted.style.display = 'none'
+  $copyButton.style.display = 'block'
+}
+
+function decryptText() {
+  const $textToDecrypt = document.querySelector('.text-to-encrypt')
+  const $decryptedTextElement = document.querySelector('.text-encrypted')
+
+  const regex = /^[a-zñ0-9.,;:!¡¿?()'"\s-]+$/
+  if ($textToDecrypt.value === '') {
+    alert('Por favor, escribe un texto para desencriptar.')
+    return
+  }
+  if (!regex.test($textToDecrypt.value)) {
+    alert('Por favor, escribe un texto válido.')
+    return
+  }
+
+  const decryptedText = decryptor($textToDecrypt.value)
+
+  $decryptedTextElement.textContent = decryptedText
+}
+
+function updateClipboard() {
+  const $encryptedTextElement = document.querySelector('.text-encrypted')
+
+  navigator.clipboard.writeText($encryptedTextElement.textContent).then(() => {
+    alert('Texto copiado al portapapeles.')
+  })
+}
